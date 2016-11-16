76243651f2a4a199f4e06c249bb95e66e5f48922
haipenge@gmail.com <haipenge@gmail.com>
2016-11-16 18:10:42 +0800

    调整菜单结构

--- src/main/webapp/js/component/app.js
+++ src/main/webapp/js/component/app.js
@@ -44,37 +44,6 @@ var Tools = {
 	},
 	config : {
 		idSeed : 0
-	},
-	/**
-	 * btn:复制按钮 targetEl:待复制内容dom(id,classs..): Exam:<button id="copy" data-clipboard-target="title" type="button"
-	 * class="btn btn-primary">Click to Copy</button> btn ->copy (id of button) <button id="copy"
-	 * data-clipboard-target="title" type="button" class="btn btn-success btn-sm"><fmt:message key="global.copy"/></button>
-	 */
-	copy : function(btn) {
-		var client = new ZeroClipboard($(btn), {
-			moviePath : '/js/lib/zeroclipboard/ZeroClipboard.swf',
-			debug : false
-		});
-		client.on('load', function(client) {
-			client.on('complete', function(client, args) {
-				var targetEl = $(btn).attr('data-clipboard-target');
-			});
-		});
-		// $(btn).mouseover(function() {
-		// $(this).tooltip({
-		// title : '点击复制'
-		// });
-		// });
-		// $(btn).click(function() {
-		// $('.tooltip-inner').empty().append('复制成功');
-		// });
-		$(btn).click(function() {
-			if ($('#copy-msg').length > 0) {
-				$('#msg').empty().append('复制成功').fadeIn(1500, function() {
-					$('#msg').fadeOut(1000);
-				});
-			}
-		});
 	}
 };
 /**
@@ -171,6 +140,7 @@ $(document).ready(function() {
 	$('.img-small').click(function(e) {
 		Img.small2Larger(this);
 	});
+	$('form').find('input:nth-child(1)').focus();
 });
 /**
  * 当通过ajax方式访问受保护页面，而又未登陆时，触发本事件，并弹出登陆对话框
@@ -346,3 +316,36 @@ Msg.prototype = {
 	}
 };
 
+var Copy={
+/**
+	 * btn:复制按钮 targetEl:待复制内容dom(id,classs..): Exam:<button id="copy" data-clipboard-target="title" type="button"
+	 * class="btn btn-primary">Click to Copy</button> btn ->copy (id of button) <button id="copy"
+	 * data-clipboard-target="title" type="button" class="btn btn-success btn-sm"><fmt:message key="global.copy"/></button>
+	 */
+	copy : function(btn) {
+		var client = new ZeroClipboard($(btn), {
+			moviePath : '/js/lib/zeroclipboard/ZeroClipboard.swf',
+			debug : false
+		});
+		client.on('load', function(client) {
+			client.on('complete', function(client, args) {
+				var targetEl = $(btn).attr('data-clipboard-target');
+			});
+		});
+		// $(btn).mouseover(function() {
+		// $(this).tooltip({
+		// title : '点击复制'
+		// });
+		// });
+		// $(btn).click(function() {
+		// $('.tooltip-inner').empty().append('复制成功');
+		// });
+		$(btn).click(function() {
+			if ($('#copy-msg').length > 0) {
+				$('#msg').empty().append('复制成功').fadeIn(1500, function() {
+					$('#msg').fadeOut(1000);
+				});
+			}
+		});
+	}
+};
